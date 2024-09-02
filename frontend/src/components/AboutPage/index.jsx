b8de9a093008be98dca413fa28d640490a5fc374
jaeahn2010 <jaeahn2010@gmail.com>
2024-09-02 01:22:12 -0700

    added styles, animations to aboutPage

--- frontend/src/components/AboutPage/index.jsx
+++ frontend/src/components/AboutPage/index.jsx
@@ -4,15 +4,15 @@ import { useState, useRef, useEffect } from 'react'
 import './styles.css'
 
 export default function AboutPage({darkTheme}) {
-    const [dragPosition, setDragPosition] = useState(0)
-    const [yearPosition, setYearPosition] = useState(0)
     const [techIndex, setTechIndex] = useState(0)
     const [techSrc, setTechSrc] = useState('')
     const [techDisplay, setTechDisplay] = useState('</>')
+    const [techCategory, setTechCategory] = useState('')
     const [coordinates, setCoordinates] = useState([51, 126])
-    const mouseStartRef = useRef(0)
-    const boundaryRef = useRef(null)
-    const barRef = useRef(null)
+    const [currentExperienceDate, setCurrentExperienceDate] = useState('Aug 2003')
+    const [svgArms, setSvgArms] = useState('')
+    const [svgLegs, setSvgLegs] = useState('')
+	const timelineRef = useRef(null)	
     const stats = [
         {
             id: 1,
@@ -46,7 +46,7 @@ export default function AboutPage({darkTheme}) {
     const experiences = [
         {
             date: 'Aug 2003',
-            event: "First took interest in coding and programming by creating simple apps on the TI-83 and TI-84 graphing calculators"
+            event: "First took interest in coding and programming by creating apps (math games and simple shooting games) on the TI-83 and TI-84 graphing calculators"
         },
         {
             date: 'Aug 2009',
@@ -70,35 +70,41 @@ export default function AboutPage({darkTheme}) {
         },
         {
             date: 'Mar 2024',
-            event: "Graduated from General Assembly: Software Engineering Immersive Program, with 4 software projects"
+            event: "Graduated from General Assembly: Software Engineering Immersive Program, with 4 software projects; also started working on more software projects, as well as accepting freelance work"
         },
         {
-            date: 'Mar 2024',
-            event: "Started working on 3 other software projects, as well as accepting freelance work"
+            date: 'Jul 2024',
+            event: "Created a schedule automation app with React that helped a summer music festival cut down on administrative costs and time"
+        },
+        {
+            date: 'Aug 2024',
+            event: "Built and designed a business website for an opera singer"
         },
     ]
+    const experienceDates = ['', '', '', ''].concat(experiences.map(experience => experience.date)).concat(['', '', '', ''])
+
     const technicalSkills = [
-        'https://img.shields.io/badge/Python-3776AB?style=for-the-badge&logo=python&logoColor=white',
-        'https://img.shields.io/badge/HTML5-E34F26?style=for-the-badge&logo=html5&logoColor=white',
-        'https://img.shields.io/badge/CSS3-1572B6?style=for-the-badge&logo=css3&logoColor=white',
-        'https://img.shields.io/badge/JavaScript-F7DF1E?style=for-the-badge&logo=javascript&logoColor=black',
-        'https://img.shields.io/badge/TypeScript-007ACC?style=for-the-badge&logo=typescript&logoColor=white',
-        'https://img.shields.io/badge/C%2B%2B-00599C?style=for-the-badge&logo=c%2B%2B&logoColor=white',
-        'https://img.shields.io/badge/Java-ED8B00?style=for-the-badge&logo=openjdk&logoColor=white',
-        'https://img.shields.io/badge/Node.js-43853D?style=for-the-badge&logo=node.js&logoColor=white',
-        'https://img.shields.io/badge/Express.js-404D59?style=for-the-badge',
-        'https://img.shields.io/badge/Django-092E20?style=for-the-badge&logo=django&logoColor=white',
-        'https://img.shields.io/badge/React-20232A?style=for-the-badge&logo=react&logoColor=61DAFB',
-        'https://img.shields.io/badge/Tailwind_CSS-38B2AC?style=for-the-badge&logo=tailwind-css&logoColor=white',
-        'https://img.shields.io/badge/Bootstrap-563D7C?style=for-the-badge&logo=bootstrap&logoColor=white',
-        'https://img.shields.io/badge/PostgreSQL-316192?style=for-the-badge&logo=postgresql&logoColor=white',
-        'https://img.shields.io/badge/MongoDB-4EA94B?style=for-the-badge&logo=mongodb&logoColor=white',
-        'https://img.shields.io/badge/Amazon_AWS-232F3E?style=for-the-badge&logo=amazon-aws&logoColor=white',
-        'https://img.shields.io/badge/GitHub-100000?style=for-the-badge&logo=github&logoColor=white',
-        'https://img.shields.io/badge/Markdown-000000?style=for-the-badge&logo=markdown&logoColor=white',
-        'https://img.shields.io/badge/Heroku-430098?style=for-the-badge&logo=heroku&logoColor=white',
-        'https://img.shields.io/badge/Flask-000000?style=for-the-badge&logo=flask&logoColor=white',
-        'https://img.shields.io/badge/Netlify-00C7B7?style=for-the-badge&logo=netlify&logoColor=white'
+        ['https://img.shields.io/badge/Amazon_AWS-232F3E?style=for-the-badge&logo=amazon-aws&logoColor=white', 'host'],
+        ['https://img.shields.io/badge/Bootstrap-563D7C?style=for-the-badge&logo=bootstrap&logoColor=white', 'frame'],
+        ['https://img.shields.io/badge/C%2B%2B-00599C?style=for-the-badge&logo=c%2B%2B&logoColor=white', 'lang'],
+        ['https://img.shields.io/badge/CSS3-1572B6?style=for-the-badge&logo=css3&logoColor=white', 'markup'],
+        ['https://img.shields.io/badge/Django-092E20?style=for-the-badge&logo=django&logoColor=white', 'frame'],
+        ['https://img.shields.io/badge/Express.js-404D59?style=for-the-badge', 'frame'],
+        ['https://img.shields.io/badge/Flask-000000?style=for-the-badge&logo=flask&logoColor=white', 'frame'],
+        ['https://img.shields.io/badge/GitHub-100000?style=for-the-badge&logo=github&logoColor=white', 'version'],
+        ['https://img.shields.io/badge/Heroku-430098?style=for-the-badge&logo=heroku&logoColor=white', 'host'],
+        ['https://img.shields.io/badge/HTML5-E34F26?style=for-the-badge&logo=html5&logoColor=white', 'markup'],
+        ['https://img.shields.io/badge/Java-ED8B00?style=for-the-badge&logo=openjdk&logoColor=white', 'lang'],
+        ['https://img.shields.io/badge/JavaScript-F7DF1E?style=for-the-badge&logo=javascript&logoColor=black', 'lang'],
+        ['https://img.shields.io/badge/Markdown-000000?style=for-the-badge&logo=markdown&logoColor=white', 'markup'],
+        ['https://img.shields.io/badge/MongoDB-4EA94B?style=for-the-badge&logo=mongodb&logoColor=white', 'db'],
+        ['https://img.shields.io/badge/Netlify-00C7B7?style=for-the-badge&logo=netlify&logoColor=white', 'host'],
+        ['https://img.shields.io/badge/Node.js-43853D?style=for-the-badge&logo=node.js&logoColor=white', 'frame'],
+        ['https://img.shields.io/badge/PostgreSQL-316192?style=for-the-badge&logo=postgresql&logoColor=white', 'db'],
+        ['https://img.shields.io/badge/Python-3776AB?style=for-the-badge&logo=python&logoColor=white', 'lang'],
+        ['https://img.shields.io/badge/React-20232A?style=for-the-badge&logo=react&logoColor=61DAFB', 'frame'],
+        ['https://img.shields.io/badge/Tailwind_CSS-38B2AC?style=for-the-badge&logo=tailwind-css&logoColor=white', 'frame'],
+        ['https://img.shields.io/badge/TypeScript-007ACC?style=for-the-badge&logo=typescript&logoColor=white', 'lang'],
     ]
 
     const xChoices = []
@@ -106,6 +112,7 @@ export default function AboutPage({darkTheme}) {
     for (let i = 51; i <= 135; i += 7) {
         xChoices.push(i)
     }
+    const btnStyle = 'border-stone-200 border-2 rounded-xl px-1 my-2 w-full hover:bg-gradient-to-r hover:from-stone-500 hover:via-stone-700 hover:to-stone-500'
 
     let sectionTitleStyle = `${!darkTheme ? 'border-stone-800' : 'border-stone-200'} section-title m-5 text-3xl text-center w-5/6 border-stone-200 border-y-2 my-12 py-5 mx-auto`
 
@@ -119,28 +126,18 @@ export default function AboutPage({darkTheme}) {
         }
     })
 
-    const handleMouseDown = (evt) => {
-        const boundary = boundaryRef.current
-        const boundaryRect = boundary.getBoundingClientRect()
-        const barRect = barRef.current.getBoundingClientRect()
-        let mouseDifference = evt.clientX - barRect.left
-        mouseStartRef.current = evt.clientX - dragPosition
-
-        const onMouseMove = (evt) => {
-            let newPos = evt.clientX - boundaryRect.left - mouseDifference
-            newPos = Math.max(0, Math.min(boundaryRect.width - 42, newPos)) // Ensure the position is within boundary
-            setDragPosition(newPos)
-            setYearPosition(newPos / boundaryRect.width)
-        }
-
-        const onMouseUp = () => {
-            document.removeEventListener("mousemove", onMouseMove)
-            document.removeEventListener("mouseup", onMouseUp)
-        }
-
-        document.addEventListener("mousemove", onMouseMove)
-        document.addEventListener("mouseup", onMouseUp)
-    }
+    function handleScrollToYear(index) {
+		if (timelineRef.current) {
+			const timelineWidth = timelineRef.current.offsetWidth
+			const yearElement = timelineRef.current.children[index]
+			const yearWidth = yearElement.clientWidth
+			const scrollLeftPosition = yearElement.offsetLeft - timelineWidth / 2 + yearWidth / 2
+			timelineRef.current.scrollTo({
+				left: scrollLeftPosition,
+				behavior: 'smooth',
+			})
+		}
+	}
 
     function handleIndex(evt) {
         evt.target.id === 'left-arrow'
@@ -148,16 +145,21 @@ export default function AboutPage({darkTheme}) {
         : setTechIndex(techIndex === technicalSkills.length - 1 ? 0 : techIndex + 1)
     }
 
+    function handleTechCategory(evt) {
+        setTechCategory(evt.target.id)
+    }
+
     useEffect(() => {
-        let techName = technicalSkills[techIndex].split('/')[4].split('-')[0].replace(/%2B/g, '+').replace(/_/g, ' ')
+        let techName = technicalSkills[techIndex][0].split('/')[4].split('-')[0].replace(/%2B/g, '+').replace(/_/g, ' ')
         for (let i = 0; i < techName.length; i++) {
             setTimeout(() => {
                 setTechDisplay(techName.slice(0, i + 1))
                 setCoordinates([xChoices[Math.floor(Math.random() * xChoices.length)], yChoices[Math.floor(Math.random() * yChoices.length)]])
             }, i * 100)
         }
-        setTechSrc(technicalSkills[techIndex])
+        setTechSrc(technicalSkills[techIndex][0])
     }, [techIndex])
+
 
     return (
         <main className={`${!darkTheme ? 'text-stone-800' : 'text-stone-200'} w-screen py-5`}>
@@ -225,16 +227,97 @@ export default function AboutPage({darkTheme}) {
                     </svg>
                     <button className='text-3xl hover:scale-125 duration-500' id='right-arrow' onClick={handleIndex}>&#xbb;</button>
                 </div>
+                <div className='flex w-11/12 items-center text-sm'>
+                    <div className='flex flex-col justify-center items-center w-1/2 mx-2'>
+                        <button id='lang' onClick={handleTechCategory} className={btnStyle}>Languages</button>
+                        <button id='frame' onClick={handleTechCategory} className={btnStyle}>Libraries/Frameworks</button>
+                        <button id='host' onClick={handleTechCategory} className={btnStyle}>Cloud Platforms/Hosting</button>
+                    </div>
+                    <div className='flex flex-col justify-center items-center w-1/2 mx-2'>
+                        <button id='db' onClick={handleTechCategory} className={btnStyle}>Databases</button>
+                        <button id='version' onClick={handleTechCategory} className={btnStyle}>Version Control</button>
+                        <button id='markup' onClick={handleTechCategory} className={btnStyle}>Markup</button>
+                    </div>
+                </div>
+                <div className='columns-3 p-2'>{technicalSkills.map(skill => 
+                    <p key={skill[0]} className={`${!techCategory || techCategory === skill[1] ? '' : 'text-stone-700'} duration-500`}>{skill[0].split('/')[4].split('-')[0].replace(/%2B/g, '+').replace(/_/g, ' ')}</p>
+                )}</div>
+
             </div>
             <h3 className={sectionTitleStyle}>EXPERIENCE</h3>
-            {/* drag bar */}
-            <div ref={boundaryRef} className={`relative w-3/4 mx-auto my-5 rounded-xl h-[20px] shadow-3xl w-[50vw] bg-gradient-to-r from-stone-500 via-stone-100 to-stone-500`}>
-                <button ref={barRef} className={`absolute w-[40px] h-[17px] top-[1.3px] rounded-full bg-gradient-to-r from-sky-500 via-sky-300 to-sky-500`} style={{left: `${dragPosition}px`}} onMouseDown={handleMouseDown}></button>
-            </div>
-            <section className='flex flex-col justify-center items-center rounded-xl my-5 mx-auto p-5 w-1/2 h-[30vh] bg-gradient-to-r from-sky-500 via-sky-200 to-sky-500'>
-                <p className='m-5 h-1/4 font-bold underline text-3xl text-stone-800'>{experiences.filter(experience => experiences.indexOf(experience) / experiences.length <= yearPosition).reverse()[0].date}</p>
-                <p className='m-5 h-3/4 text-xl text-stone-800'>{experiences.filter(experience => experiences.indexOf(experience) / experiences.length <= yearPosition).reverse()[0].event}</p>
-            </section>
+            <div className='min-h-[25vh]'>
+				<div
+					ref={timelineRef}
+					className='scrollbar-hide overflow-x-scroll flex w-full max-w-5xl my-5 p-4 space-x-4 snap-x snap-mandatory border-stone-200 border-y-2 bg-gradient-to-r from-stone-400 via-stone-600 to-stone-400'
+				>
+					{experienceDates.map((date, index) => {
+						return date
+						? <button
+							key={date}
+							onClick={() => {
+								// isMobile ? handleScrollToYear(index) : handleScrollToYear(index - 2)
+								// setCurrentYear(year)
+                                handleScrollToYear(index)
+                                setCurrentExperienceDate(date)
+							}}
+							className={`flex-none w-24 hover:scale-150 duration-500 flex items-center justify-center text-lg md:text-3xl font-bold snap-center ${date === currentExperienceDate ? 'text-stone-100' : 'text-stone-350'}`}
+						>{date}</button>
+						: <div key={index} className="flex-none w-24"></div>
+					})}
+				</div>
+                <div>
+                    {experiences.filter(experience => experience.date === currentExperienceDate).map(experience => 
+                    <>
+                        <div className={`${!darkTheme ? 'border-stone-900' : 'border-stone-200'} border-y-4 h-[30vh] flex justify-center items-center`}>
+                        <svg width="375" height="200" viewBox="0 0 375 200" xmlns="http://www.w3.org/2000/svg">
+                            {/* running figure */}
+                            {/* head */}
+                            <rect x='50' y='105' width='20' height='20' fill='#cccccc' rx='10' ry='10'/>
+                            {/* eyes */}
+                            <rect x='63' y='110' width='3' height='3' fill='#000000'/>
+                            <rect x='58' y='110' width='3' height='3' fill='#000000'/>
+                            {/* neck */}
+                            <rect x='57' y='125' width='6' height='3' fill='#cccccc'/>
+                            {/* torso */}
+                            <rect x='48' y='128' width='24' height='33' fill='#cccccc' rx='6' ry='8'/>
+                            {/* right arm */}
+                            <polygon points='48,137 38,137 38,154 43,154 43,145 48,145' fill='#cccccc'/>
+                            {/* left arm */}
+                            <polygon points='72,140 72,148 82,148 82,131 77,131 77,140' fill='#cccccc'/>
+                            {/* left leg */}
+                            <polygon points='51,160 57,160 57,174 43,174 43,168 51,168' fill='#cccccc'/>
+                            {/* right leg */}
+                            <polygon points='60,160 68,160 76,170 68,180 60,180 68,170' fill='#cccccc'/>
+                            {/* mountains far */}
+                            <polyline points='0,60 60,0 120,60' fill='none' stroke='#ffffff' strokeLinecap='round' strokeWidth='2'/>
+                            <polyline points='100,40 140,0 200,60' fill='none' stroke='#ffffff' strokeLinecap='round' strokeWidth='2'/>
+                            <polyline points='180,40 220,0 280,60' fill='none' stroke='#ffffff' strokeLinecap='round' strokeWidth='2'/>
+                            <polyline points='260,40 300,0 360,60' fill='none' stroke='#ffffff' strokeLinecap='round' strokeWidth='2'/>
+                            <polyline points='340,40 380,0' fill='none' stroke='#ffffff' strokeLinecap='round' strokeWidth='2'/>
+                            {/* cactus 1 far */}
+                            <polygon points='145,110 150,110 150,95 160,95 160,75 155,75 155,90 150,90 150,60 145,60 145,90 140,90 140,70 135,70 135,95 145,95' fill='#cccccc'/>
+                            {/* cactus 2 far */}
+                            <polygon points='175,110 180,110 180,93 185,93 185,63 179,63 179,88 176,88 176,74 170,74 170,93 175,93' fill='#cccccc'/>
+                            {/* rocks 1 far */}
+                            <polygon points='164,99 173,101 180,112 163,112' fill='#cccccc' stroke='#000000'/>
+                            <polygon points='161,112 169,112 165,105 162,105' fill='#cccccc' stroke='#000000'/>
+                            {/* rocks 2 far */}
+                            <polygon points='360,99 369,101 376,112 359,112' fill='#cccccc' stroke='#000000'/>
+                            <polygon points='357,112 365,112 361,105 358,105' fill='#cccccc' stroke='#000000'/>
+                            {/* cactus near */}
+                            <polygon points='245,180 255,180 255,160 270,160 270,120 260,120 260,150 255,150 255,110 245,110 245,150 240,150 240,130 230,130 230,160 245,160' fill='#cccccc'/>
+                            {/* rocks near */}
+                            <polygon points='315,180 320,180 317,170 316,170' fill='#cccccc' stroke='#000000'/>
+                            <polygon points='285,180 315,180 308,160 290,163' fill='#cccccc' stroke='#000000'/>
+                        </svg>
+                        </div>
+                        <div className='text-justify m-5 p-3 border-stone-200 border-2 rounded-3xl bg-gradient-to-r from-stone-500 via-stone-700 to-stone-500'>
+                            <p>{experience.event}</p>
+                        </div>
+                    </>
+                    )}
+                </div>
+			</div>
         </main>
     )
 }
